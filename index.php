af6e90014cdb6809dcba2814fb141f687ba96512
umiyosh <yumino99@gmail.com>
2015-09-12 12:51:31 +0900

    enable to work praise api dummy
     index.php | 10 ++++++++--
     1 file changed, 8 insertions(+), 2 deletions(-)

--- index.php
+++ index.php
@@ -8,10 +8,16 @@ $app->get('/', function () { //as closure
     echo 'Hello World';
 });
 
-$app->get('/message/:name', 'messageGet'); //classic php style with function name
+$app->get('/message/:name', 'messageGet');
+$app->post('/praise/', 'praiseAdd');
 
 function messageGet($name) {
-    $respJson=sprintf('{{user_name: %s, "message":["hoge", "fuga"]}', $name);
+    $respJson=sprintf('{"status": "success", "user_name": %s, "message":["hoge", "fuga"]}', $name);
+    echo $respJson;
+}
+
+function praiseAdd() {
+    $respJson=sprintf('{"status": "success","user_name": "hoge"}');
     echo $respJson;
 }
 
