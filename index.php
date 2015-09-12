a1540b589f3c7ec1bf2da78f77bf586cdbe134d5
umiyosh <yumino99@gmail.com>
2015-09-12 14:40:59 +0900

    rformated

--- index.php
+++ index.php
@@ -7,32 +7,55 @@ $app = new \Slim\Slim();
 $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
 
-$app->get('/', function() use ($app) {
-    $app->render(200,array(
-            'msg' => 'welcome to PraiseAPi!',
-        ));
-});
+$app->get(
+    '/',
+    function () use ($app) {
+        $app->render(
+            200,
+            array(
+                'msg' => 'welcome to PraiseAPi!',
+            )
+        );
+    }
+);
 
-$app->get('/message/', function() use ($app) { messageGet($app); });
-$app->post('/praise/', function() use ($app){ praiseAdd($app);});
+$app->get(
+    '/message/',
+    function () use ($app) {
+        messageGet($app);
+    }
+);
+$app->post(
+    '/praise/',
+    function () use ($app) {
+        praiseAdd($app);
+    }
+);
 
 $app->get('/functest', 'selectMessages');
 
-function messageGet($app) {
-  $msg = selectMessages();
-  $app->render(200, array(
-      'user_name' => 'hoge',
-      'message'   => $msg
-    )
-  );
-}
-
-function praiseAdd($app) {
-    $app->render(200, array(
-        'user_name' => 'hoge'
-      )
+function messageGet($app)
+{
+    $msg = selectMessages();
+    $app->render(
+        200,
+        array(
+            'user_name' => 'hoge',
+            'message' => $msg
+        )
     );
 }
+
+function praiseAdd($app)
+{
+    $app->render(
+        200,
+        array(
+            'user_name' => 'hoge'
+        )
+    );
+}
+
 $app->run();
 
 function selectMessages()
@@ -43,7 +66,7 @@ function selectMessages()
     // メッセージ数確認
     $messagesLength = count($messages);
 
-    $selected = rand(0, $messagesLength-1);
+    $selected = rand(0, $messagesLength - 1);
 
     return $messages[$selected];
 }
