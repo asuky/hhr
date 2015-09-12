31fdeb8b2cee936dddf8d34c22f40d873a4a98d4
Asuki YAMASHITA <asuky.y@gmail.com>
2015-09-12 04:08:10 +0000

    messages.txtを読み込み、ランダムに返す関数 selectMessages を作りました。

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+require_once 'consts.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
@@ -9,9 +10,22 @@ $app->get('/', function () { //as closure
 });
 
 $app->get('/message', 'helloName'); //classic php style with function name
+$app->get('/functest', 'selectMessages');
 
 function helloName($name) {
     echo "Hello $name";
 }
-
 $app->run();
+
+function selectMessages()
+{
+    // メッセージを全部呼びこむ
+    $messages = file(__DIR__ . DS . MESSAGES_FILENAME);
+    
+    // メッセージ数確認
+    $messagesLength = count($messages);
+    
+    $selected = rand(0, $messagesLength-1);
+    
+    return $messages[$selected];
+}
